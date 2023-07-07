a82c1336256996d77db967ccc9766fbcd21bbe5c
Vladimir Bushev <voldemarbush@gmail.com>
2023-07-07 10:50:37 +0300

    feat: add english secttion

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const body = document.querySelector('body');
 const navBar = document.querySelector('.cv__navbar');
 const menu = document.querySelector('.cv__navbar-menu');
 const burgerIcon = document.querySelector('.cv__navbar-menu_icon');
 const burger = document.querySelector('.cv__navbar-burger');
 
 burger.addEventListener('click', function () {
+  body.classList.toggle('active');
   navBar.classList.toggle('expand');
   burgerIcon.classList.toggle('active');
   menu.classList.toggle('close');
