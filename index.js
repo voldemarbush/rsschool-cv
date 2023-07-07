83e958ebca0caee3f5df53b0c0507351a5338cae
Vladimir Bushev <voldemarbush@gmail.com>
2023-07-07 09:02:03 +0300

    feat: add skills secttion

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const menu = document.querySelector('.cv__navbar-menu');
 const burgerIcon = document.querySelector('.cv__navbar-menu_icon');
 const burger = document.querySelector('.cv__navbar-burger');
 
-console.log(menu, navBar, burger);
-
 burger.addEventListener('click', function () {
   navBar.classList.toggle('expand');
   burgerIcon.classList.toggle('active');
